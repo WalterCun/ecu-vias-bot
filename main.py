45075441dc21eaf8e7b97e4243c5deca2b263390
WalterCun <waltercunbustamante@gmail.com>
2025-01-27 16:57:45 -0500

    Add main.py to initialize and start the bot
    This script sets up logging and launches the bot using `start_bot`. It includes customizable logging configurations and optional memory tracing (commented out).

---
+++ main.py
@@ -0,0 +1,22 @@
+""" main.py """
+import logging
+# import tracemalloc
+
+from src.bot import start_bot
+
+# Enable logging
+logging.basicConfig(
+    format="<%(lineno)d> [%(asctime)s] - [%(filename)s / %(funcName)s] - [%(levelname)s] -> %(message)s",
+    level=logging.INFO
+)
+
+# set a higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    # tracemalloc.start()
+    start_bot()
+    # snapshot = tracemalloc.take_snapshot()
+    # top_stats = snapshot.statistics('lineno')
