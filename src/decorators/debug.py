2f95eeade6a84a7e35fa8aaf2a05920093693e98
WalterCun <waltercunbustamante@gmail.com>
2025-01-27 22:18:07 -0500

    Add debug decorator to log function names and results
    Introduce a `print_result` decorator to log the function name and its output when executed. Supports both synchronous and asynchronous functions, and is conditionally enabled based on the DEBUG setting.

---
+++ src/decorators/debug.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" debug.py """
+import inspect
+
+from src.settings.config import settings
+
+
+def print_result(func):
+    """
+    A decorator that prints the name of the function and its result after
+    the function is executed.
+
+    Args:
+        func (function): The function to be wrapped by the decorator.
+
+    Returns:
+        function: The wrapper function that adds the printing functionality.
+    """
+
+    async def async_wrapper(*args, **kwargs):
+        """
+        A decorator that prints the name of the function and its result after
+        the function is executed.
+
+        Args:
+            func (function): The function to be wrapped by the decorator.
+
+        Returns:
+            function: The wrapper function that adds the printing functionality.
+        """
+        result = await func(*args, **kwargs)
+        print(func.__name__, result)
+
+    def sync_wrapper(*args, **kwargs):
+        """
+        A wrapper for sync functions.
+        """
+        result = func(*args, **kwargs)
+        print(func.__name__, result)
+
+    if settings.DEBUG:
+        if inspect.iscoroutinefunction(func):
+            return async_wrapper
+        else:
+            return sync_wrapper
+
+    return func
