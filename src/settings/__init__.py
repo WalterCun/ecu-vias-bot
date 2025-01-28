3eff7cd34329c65ea28217c62d350dda7315fca3
WalterCun <waltercunbustamante@gmail.com>
2025-01-28 18:55:59 -0500

    Refactor settings module and improve configuration structure
    Reorganized `settings` by consolidating configurations into `__init__.py`. Removed unused variables, enhanced type annotations, and introduced SQLite for database configuration. Improved readability and maintainability by cleaning up redundant imports and aligning with best practices.

---
+++ src/settings/__init__.py
@@ -0,0 +1,71 @@
+"""src/config/settings.py"""
+
+from pathlib import Path
+from pydantic_settings import BaseSettings
+from datetime import time
+
+
+class Settings(BaseSettings):
+    """
+    Settings class used for managing configuration settings.
+
+    Attributes:
+        DEBUG (bool): A flag indicating if the debug mode is enabled.
+        LOCALES_PATH (str): Path for locale files.
+        MODERATOR (int): Index for the moderator type.
+        SUBSCRIPTION (int): Index for the subscription type.
+        NOTIFICATIONS (int): Index for the notification type.
+        ALARM_NOTIFICATIONS (int): Index for the alarm notifications type.
+        UNSUBSCRIPTION (int): Index for the unsubscription type.
+        CONFIG (int"""
+    DEBUG: bool
+    BASE_DIR: Path = Path(__file__).parent.parent.parent
+    # Fluid
+    LOCALES_PATH: Path = BASE_DIR / 'src' / 'controller' / 'languages' / 'langs'
+
+    # States indexes for conversation handler
+    MODERATOR: int = 0
+    SUBSCRIPTION: int = 1
+    NOTIFICATIONS: int = 2
+    ALARM_NOTIFICATIONS: int = 3
+    UNSUBSCRIPTION: int = 4
+    CONFIG: int = 5
+    CANCEL: int = 6
+    UNSUBSCRIBE_FAIL: int = 7
+
+    DB_NAME: str = 'ecuavias.db'
+    DATABASE_URL: str = f'sqlite://{BASE_DIR / DB_NAME}'
+    REDIS_URL: str = 'redis://localhost:6379/0' # Opcional
+
+    # Bot Telegram
+    TELEGRAM_KEY_BOT: str
+    ID_ADMIN: int
+
+    # Web Scrapping
+    URL_SCRAPPING_WEB: str
+    DAYS_TO_KEEP_DATA: str
+
+    # Cache Requests
+    CACHE_FILE: str
+    CACHE_TIME_EXPIRATION: int = 3600
+
+    ONCE_A_DAY: dict[str, time] = {
+        f"{hour % 12 or 12} {'AM' if hour < 12 else 'PM'}": time(hour=hour) for hour in range(24)
+    }
+
+    class Config:
+        """
+
+        Config is a class used to manage environment configuration settings.
+
+        Attributes:
+            env_file (str): The name of the environment file to load.
+        """
+        env_file = str(Path(__file__).parent.parent.parent / ".env")
+
+
+settings = Settings()
+
+if __name__ == '__main__':
+    print(settings.ONCE_A_DAY
+          )
--- src/settings/const.py
+++ src/settings/const.py
@@ -1,18 +1,30 @@
-from datetime import time
+# from datetime import time
+#
+# from src.controller.languages.translations import language
+#
+# ONCE_A_DAY: dict[str, time] = {
+#     f"{hour % 12 or 12} {'AM' if hour < 12 else 'PM'}": time(hour=hour) for hour in range(24)
+# }
+#
+from src.translations.core import translate
 
-from src.controller.languages.translations import language
+# HEADERS = {
+#     "User-Agent": (
+#         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+#         "Chrome/108.0.0.0 Safari/537.36"
+#     ),
+#     "Accept-Language": "en-US,en;q=0.9",
+#     "Content-Type": "application/json"
+# }
 
-ONCE_A_DAY: dict[str, time] = {
-    f"{hour % 12 or 12} {'AM' if hour < 12 else 'PM'}": time(hour=hour) for hour in range(24)
-}
-
-SEVERAL_TIMES_A_DAY = [
-    language.times_one_hour,
-    language.times_three_hour,
-    language.times_six_hour
+SEVERAL_TIMES_A_DAY: list = [
+    translate.times_one_hour,
+    translate.times_three_hour,
+    translate.times_six_hour
 ]
 
-PROVINCES = ['Azuay', 'Bolivar', 'Cañar', 'Carchi', 'Chimborazo', 'Cotopaxi', 'El Oro', 'Esmeraldas', 'Galápagos',
-             'Guayanas', 'Imbabura', 'Loja', 'Los Ríos', 'Manabí', 'Morona Santiago', 'Napo', 'Sucumbíos',
-             'Pastaza', 'Pinchincha', 'Santa Elena', 'Santo Domingo de los Tsáchilas', 'Francisco de Orellana',
-             'Tungurahua', 'Zamora Chinchipe']
+PROVINCES: list[str] = [
+    'Azuay', 'Bolivar', 'Cañar', 'Carchi', 'Chimborazo', 'Cotopaxi', 'El Oro', 'Esmeraldas', 'Galápagos', 'Guayaquil',
+    'Imbabura', 'Loja', 'Los Ríos', 'Manabí', 'Morona Santiago', 'Napo', 'Sucumbíos', 'Pastaza', 'Pinchincha',
+    'Santa Elena', 'Santo Domingo de los Tsáchilas', 'Francisco de Orellana', 'Tungurahua', 'Zamora Chinchipe'
+]
