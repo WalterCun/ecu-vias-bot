7cfa66ec26de056e1aa5a9efc581c94d325a9c61
WalterCun <waltercunbustamante@gmail.com>
2025-08-11 23:51:11 -0500

    feat(translate): introduce translation loader with Spanish as default language

---
+++ bot/libs/translate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""  """
+from pathlib import Path
+
+from translator import Translator
+
+from bot.settings import settings
+
+trans = Translator(
+    translations_dir=Path(settings.BASE_DIR) / 'locale',
+    default_lang='es',
+    nested=True,
+    auto_add_missing_keys=True
+)
+
+# if __name__ == '__main__':
+#     print(trans.start.start.welcome)
